3e97b84e34e7bf277b1e90139ba5e4c5672d8f3e
Priyanshu <ps.rathore.ac@gmail.com>
2020-11-20 16:28:40 +0530

    muski k liye kaam

--- lib/face.dart
+++ lib/face.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
 class face extends StatefulWidget {
   @override
@@ -6,11 +9,93 @@ class face extends StatefulWidget {
 }
 
 class _faceState extends State<face> {
+  File pickedImage;
+  var text = '';
+
+  bool imageLoaded = false;
+
+  Future pickImage() async {
+    var awaitImage = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      pickedImage = awaitImage;
+      imageLoaded = true;
+    });
+
+    FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(pickedImage);
+
+    final ImageLabeler cloudLabeler =
+    FirebaseVision.instance.cloudImageLabeler();
+
+    final List<ImageLabel> cloudLabels =
+    await cloudLabeler.processImage(visionImage);
+
+    for (ImageLabel label in cloudLabels) {
+      final double confidence = label.confidence;
+      setState(() {
+        text = "$text $label.text   $confidence.toStringAsFixed(2) \n";
+
+        print(text);
+      });
+    }
+
+    cloudLabeler.close();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        'hello',
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          SizedBox(height: 100.0),
+          imageLoaded
+              ? Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: const [
+                    BoxShadow(blurRadius: 20),
+                  ],
+                ),
+                margin: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                height: 250,
+                child: Image.file(
+                  pickedImage,
+                  fit: BoxFit.cover,
+                ),
+              ))
+              : Container(),
+          SizedBox(height: 10.0),
+          Center(
+            child: FlatButton.icon(
+              icon: Icon(
+                Icons.photo_camera,
+                size: 100,
+              ),
+              label: Text(''),
+              textColor: Theme
+                  .of(context)
+                  .primaryColor,
+              onPressed: () async {
+                pickImage();
+              },
+            ),
+          ),
+          SizedBox(height: 10.0),
+          SizedBox(height: 10.0),
+          text == ''
+              ? Text('Text will display here')
+              : Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                  text,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'face.dart';
